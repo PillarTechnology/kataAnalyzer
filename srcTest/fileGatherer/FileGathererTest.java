15628dc08830b0a55d0da6b539b65ae346ca904f
NDUB <ndubetz@pillartechnology.com>
2016-03-16 14:35:22 -0400

    Created a set of files to be used for testing. Refactored the one test to use this set.

--- srcTest/fileGatherer/FileGathererTest.java
+++ srcTest/fileGatherer/FileGathererTest.java
@@ -1,34 +1,29 @@
 package fileGatherer;
 
-import junit.framework.TestCase;
 import org.junit.Test;
 
 import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import static org.junit.Assert.assertEquals;
 
-public class FileGathererTest extends TestCase {
+public class FileGathererTest{
 
     @Test
+    @SuppressWarnings("ConstantConditions")
     public void testTakesAListOfFilesAndReturnsListOfJavaFiles() throws Exception{
-        Path path = Paths.get("/testResources/");
+        String PATH_TO_TEST_FILES = "testResources";
+        File file = new File(PATH_TO_TEST_FILES);
+        List<File> listOfJavaFiles = Arrays.asList(file.listFiles());
 
-        final File notAJavaFile = new File(path.toString(),"readme.txt");
-        final File notAJavaFile2 = new File(path.toString(),"readme2.txt");
-        final File aJavaFile1 = new File(path.toString(),"readme.java");
-        final File aJavaFile2 = new File(path.toString(),"readmeagain.java");
-        final File aJavaFile3 = new File(path.toString(),"dontreadme.java");
-        final File notAJavaFileThatHasDotJava = new File(path.toString(),"read.java.me.txt");
-
-        List<File> filesPassedIn = Arrays.asList(notAJavaFile, notAJavaFile2, aJavaFile1,
-                aJavaFile2, aJavaFile3, notAJavaFileThatHasDotJava);
+        final File aJavaFile1 = new File(PATH_TO_TEST_FILES,"AnActualJavaTest.java");
+        final File aJavaFile2 = new File(PATH_TO_TEST_FILES,"AnotherActualJavaTest.java");
+        final File aJavaFile3 = new File(PATH_TO_TEST_FILES,"NotAJavaTestClass.java");
         List<File> expectedFiles = Arrays.asList(aJavaFile1, aJavaFile2, aJavaFile3);
 
         FileGatherer gatherer = new FileGatherer();
 
-        List<File> listOfFilesReturned = gatherer.filterJavaFiles(filesPassedIn);
+        List<File> listOfFilesReturned = gatherer.filterJavaFiles(listOfJavaFiles);
         assertEquals(expectedFiles, listOfFilesReturned);
     }
 }
