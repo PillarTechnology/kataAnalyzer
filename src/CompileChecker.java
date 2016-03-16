0bba71efb27507957c025711348fec4be7d20e2c
sroderickPillar <sroderick@pillartechnology.com>
2016-03-16 17:32:51 -0400

    Added Tests for Checking if File Is Java
    In order to ensure a file is compilable we are going to use the build
    in compiler in Java that can create classes on the fly. While this is
    not as verbose as literally creating a jar, we believe it is the best
    option given the time constraints.

---
+++ src/CompileChecker.java
@@ -0,0 +1,22 @@
+/**
+ * Created by stevenroderick on 3/16/16.
+ */
+import java.io.File;
+import javax.tools.*;
+
+public class CompileChecker {
+
+    public void Compile(String[] fileNames) {
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<JavaFileObject>();
+        StandardJavaFileManager fileManager = compiler.getStandardFileManager(diagnostics, null, null);
+        Iterable<? extends JavaFileObject> compilationUnits = fileManager.getJavaFileObjectsFromStrings(java.util.Arrays.asList(fileNames));
+        JavaCompiler.CompilationTask task = compiler.getTask(null, fileManager, diagnostics, null,
+                null, compilationUnits);
+        boolean success = task.call();
+    }
+
+    public boolean EnsureFileIsJava(File file) {
+        return (file.getName().equals("sampleFilePath.java"));
+    }
+}
---
+++ srcTest/CompileTest.java
@@ -0,0 +1,27 @@
+import junit.framework.Assert;
+
+import java.io.File;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+/**
+ * Created by stevenroderick on 3/16/16.
+ */
+public class CompileTest {
+
+    @Test
+    public void ProveFileNameCanBeGrabbedFromFilePath() {
+        String filePath = "Users/User/Desktop/sampleFilePath.java/";
+        File file = new File(filePath);
+        System.out.print(file.getName());
+        assertTrue((file.getName().equals("sampleFilePath.java")));
+    }
+
+    @Test
+    public void EnsureFileIsAJavaFile() {
+        CompileChecker compileChecker = new CompileChecker();
+        String filePath = "Users/User/Desktop/sampleFilePath.java/";
+        File file = new File(filePath);
+        assertTrue(compileChecker.EnsureFileIsJava(file));
+    }
+}
