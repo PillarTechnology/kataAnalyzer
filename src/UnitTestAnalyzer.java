11c4a95120c2482386c67a2dd17aa80b59ee2010
jewinters <jewinters@gmail.com>
2016-03-17 10:43:08 -0400

    Adding utility classes to run unit tests and analyze results

---
+++ src/UnitTestAnalyzer.java
@@ -0,0 +1,23 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by jwinters on 3/16/16.
+ */
+
+public class UnitTestAnalyzer {
+
+    public String analyzeResults(String unitTestResults) {
+
+        Pattern pattern = Pattern.compile(".*OK.*\\d+\\stests?.*", Pattern.DOTALL);
+        Matcher matcher = pattern.matcher(unitTestResults);
+
+        String analysisResult = "Failure";
+
+        if (matcher.matches()) {
+            analysisResult = "Success";
+        }
+
+        return analysisResult;
+    }
+}
