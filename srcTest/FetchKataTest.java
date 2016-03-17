925ed136171c85eeb905394bf789370851d3ec8d
sroderickPillar <sroderick@pillartechnology.com>
2016-03-17 16:42:58 -0400

    Added code to pull Kata Repo from git
    Added a class and a test class to make sure we could cleanly pull down
    a repo and clean it up.

---
+++ srcTest/FetchKataTest.java
@@ -0,0 +1,38 @@
+import org.junit.Test;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by stevenroderick on 3/17/16.
+ */
+public class FetchKataTest {
+
+    @Test
+    public void itCreatesLocalRepoWhenGivenAURL() {
+        FetchKata fetchKata = new FetchKata();
+        String directory = fetchKata.cloneRepository("https://github.com/PillarTechnology/kataAnalyzer");
+        File dir = new File(directory);
+        assertTrue(dir.exists());
+    }
+
+    @Test
+    public void itWillCleanUpLocalRepoWhenCalled() {
+        FetchKata fetchKata = new FetchKata();
+        fetchKata.cleanup();
+    }
+
+    @Test
+    public void itWillDeleteLocalRepoWhenGivenADirectory() {
+        FetchKata fetchKata = new FetchKata();
+        String repo = System.getProperty("user.dir") + "/SHOULD_BE_DELETED";
+        File newFile = new File(repo);
+        newFile.mkdir();
+        fetchKata.deleteDirectory(newFile);
+        assertFalse(newFile.exists());
+    }
+}
