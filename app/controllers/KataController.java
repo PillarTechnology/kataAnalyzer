2816e6c27d192c79acb0e130cd4868e8470f8db8
Zack Brown <zbrown@pillartechnology.com>
2016-03-17 10:57:01 -0400

    A candidate can submit a kata
    * candidate can submit name, kata name, email, recruiter email and kata url
    * added Java play web framework
    * tests for Kata model
    * KataController create() is the entry point for running code quality modules

---
+++ app/controllers/KataController.java
@@ -0,0 +1,28 @@
+package controllers;
+
+import models.Kata;
+
+import play.data.Form;
+import play.data.FormFactory;
+import play.mvc.Result;
+import play.mvc.*;
+import views.html.success;
+
+import javax.inject.Inject;
+
+public class KataController extends Controller {
+
+    public FormFactory formFactory;
+
+    @Inject
+    public KataController(FormFactory formFactory) {
+        this.formFactory = formFactory;
+    }
+
+
+    public Result create() {
+        Form<Kata> form = formFactory.form(Kata.class).bindFromRequest();
+        return ok(success.render(form.get()));
+    }
+
+}
