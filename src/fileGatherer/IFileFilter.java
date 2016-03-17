e60410819398f8e9006827ca1815c17f566c2cdf
DNixonLLC <derek.nixon.llc@gmail.com>
2016-03-17 15:43:48 -0400

    Ignored test resources, added upper layer to sort files given a directory name only

---
+++ src/fileGatherer/IFileFilter.java
@@ -0,0 +1,7 @@
+package fileGatherer;
+
+/**
+ * Created by dnixon on 3/17/16.
+ */
+public interface IFileFilter {
+}
---
+++ src/fileGatherer/SourceFileGatherer.java
@@ -0,0 +1,15 @@
+package fileGatherer;
+
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
+
+public class SourceFileGatherer {
+
+    public HashMap<SourceType, List<File>> Gather(Path sourcePath) {
+        return null;
+    }
+
+}
