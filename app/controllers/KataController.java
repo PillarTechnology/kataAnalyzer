7b18ab091d10f25c60fa98d1663b0b96042dc45e
DNixonLLC <derek.nixon.llc@gmail.com>
2016-03-18 11:28:14 -0400

    Uncommented lines temporarily pulled out to allow build

--- app/controllers/KataController.java
+++ app/controllers/KataController.java
@@ -1,12 +1,12 @@
 package controllers;
 
-//import models.Kata;
+import models.Kata;
 
-//import play.data.Form;
+import play.data.Form;
 import play.data.FormFactory;
-//import play.mvc.Result;
+import play.mvc.Result;
 import play.mvc.*;
-//import views.html.success;
+import views.html.success;
 
 import javax.inject.Inject;
 
@@ -18,11 +18,9 @@ public class KataController extends Controller {
     public KataController(FormFactory formFactory) {
         this.formFactory = formFactory;
     }
-
-
-//    public Result create() {
-//        Form<Kata> form = formFactory.form(Kata.class).bindFromRequest();
-//        return ok(success.render(form.get()));
-//    }
+    public Result create() {
+        Form<Kata> form = formFactory.form(Kata.class).bindFromRequest();
+        return ok(success.render(form.get()));
+    }
 
 }
